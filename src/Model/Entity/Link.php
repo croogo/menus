0317c47790d34f5ebca144817db21adcb7abbba9
Rachman Chavik <rchavik@xintesa.com>
2017-03-27 22:59:42 +0700

    Port Translate plugin

---
+++ src/Model/Entity/Link.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Croogo\Menus\Model\Entity;
+
+use Cake\ORM\Behavior\Translate\TranslateTrait;
+use Cake\ORM\Entity;
+
+class Link extends Entity
+{
+
+    use TranslateTrait;
+
+}
